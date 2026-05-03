55f6500223199933c99ddad3e521620a91ff11fe
ArulDevLabs <opbotpiro@gmail.com>
2026-05-04 00:54:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+function addGoal() {
+  const input = document.getElementById('goalInput');
+  const list = document.getElementById('goalList');
 
+  if(input.value.trim() !== '') {
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    list.appendChild(li);
+    input.value = '';
+  }
+}
+
+let time = 1500;
+let running = false;
+
+function startTimer() {
+  if(running) return;
+
+  running = true;
+
+  const timerDisplay = document.getElementById('timer');
+
+  const interval = setInterval(() => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    timerDisplay.textContent =
+      `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+
+    time--;
+
+    if(time < 0) {
+      clearInterval(interval);
+      timerDisplay.textContent = 'Done!';
+      running = false;
+    }
+  }, 1000);
+}
